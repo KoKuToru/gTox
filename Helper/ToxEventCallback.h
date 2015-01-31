d258c2783bb5f9bcf30a0c1f88d16cc9fb375b02
Luca Béla Palkovics <luca.bela.palkovics@gmail.com>
2015-01-31 13:11:13 +0100

    Added a class to manage events

---
+++ Helper/ToxEventCallback.h
@@ -0,0 +1,28 @@
+#ifndef TOXEVENTCALLBACK_H
+#define TOXEVENTCALLBACK_H
+
+#include <mutex>
+#include <functional>
+#include "Tox/Tox.h"
+
+class ToxEventCallback {
+    public:
+        typedef std::function<void(const Tox::SEvent&)> EFunc;
+
+        ToxEventCallback();
+        ToxEventCallback(const EFunc& func);
+        ToxEventCallback(const ToxEventCallback& o);
+        ~ToxEventCallback();
+
+        void operator=(const EFunc& func);
+        void operator=(const ToxEventCallback& o);
+
+        static void notify(const Tox::SEvent& data);
+
+    private:
+        static std::recursive_mutex m_mutex;
+        static std::vector<EFunc*> m_callback_list;
+        EFunc m_callback;
+};
+
+#endif
