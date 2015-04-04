f0531fa3c332545579bee85cb05cb0a2f3bb32d8
Maurice Mohlek <mohlek@gmail.com>
2015-04-04 19:47:41 +0200

    Empty return send no empty message

--- Widget/WidgetChat.cpp
+++ Widget/WidgetChat.cpp
@@ -189,6 +189,11 @@ WidgetChat::WidgetChat(Tox::FriendNr nr)
                 text += gunichar(0xFDD0);
             }
 
+            bool allow_send = text.find_first_not_of(" \t\n\v\f\r") != std::string::npos;
+
+            if(!allow_send)
+                return;
+
             Tox::instance().send_message(get_friend_nr(), text);
 
             // add to chat
