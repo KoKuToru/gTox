026d61a86bf6d1f6d8ce099fac1ac73dc89e11a8
Luca Béla Palkovics <luca.bela.palkovics@gmail.com>
2015-07-06 18:45:24 +0200

    toxmm-rewrite: chat_message

---
+++ src/widget/chat_message.cpp
@@ -0,0 +1,31 @@
+/**
+    gTox a GTK-based tox-client - https://github.com/KoKuToru/gTox.git
+
+    Copyright (C) 2015  Luca Béla Palkovics
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+**/
+#include "chat_message.h"
+
+using namespace widget;
+
+chat_message::chat_message(const Glib::ustring& text):
+    m_label(text) {
+    show();
+    add(m_label);
+    property_reveal_child() = false;
+    m_dispatcher.emit([this]() {
+        property_reveal_child() = true;
+    });
+}
