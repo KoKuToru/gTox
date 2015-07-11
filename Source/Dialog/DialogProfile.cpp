3abf88ecc2b9c78e2db02daf52d3e43ce3efa570
Spagy <Spagy@users.noreply.github.com>
2015-07-11 17:14:22 +0100

    Check if there is a filemanager that handles URIs
    This stops a segfault if when opening trying to open a profile in
    a file manager when there is no filemanager that handles URIs

--- Source/Dialog/DialogProfile.cpp
+++ Source/Dialog/DialogProfile.cpp
@@ -99,11 +99,11 @@ DialogProfile::DialogProfile(BaseObjectType* cobject, gToxBuilder builder, const
     open_in_fm->signal_activate().connect([this, profile_list](){
         auto row = profile_list->get_selected_row();
         if (row) {
-            try {
-                Gio::AppInfo::get_default_for_type("inode/directory", true)->launch_uri(Glib::filename_to_uri(m_accounts[row->get_index()]));
-            } catch (...) {
-                //TODO: display error !
-            }
+            auto filemanager = Gio::AppInfo::get_default_for_type("inode/directory",
+                    true);
+            if (filemanager)
+                filemanager->launch_uri(
+                        Glib::filename_to_uri(m_accounts[row->get_index()]));
         }
     });
 
