290a5272fd99db6880c1bcd5bde0e687fc53f785
Luca Béla Palkovics <luca.bela.palkovics@gmail.com>
2015-08-08 18:38:49 +0200

    toxmm-rewrite: videoplayer

---
+++ src/widget/imagescaled.h
@@ -0,0 +1,47 @@
+/**
+    gTox a GTK-based tox-client - https://github.com/KoKuToru/gTox.git
+
+    Copyright (C) 2015  Luca Béla Palkovics
+    Copyright (C) 2015  Maurice Mohlek
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+**/
+#ifndef WIDGETIMAGESCALED_H
+#define WIDGETIMAGESCALED_H
+
+#include <gtkmm.h>
+#include "utils/builder.h"
+#include "utils/dispatcher.h"
+
+namespace widget {
+    class imagescaled : public Gtk::Image {
+        public:
+            imagescaled(BaseObjectType* cobject,
+                   utils::builder);
+            ~imagescaled();
+
+        protected:
+            bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
+            Gtk::SizeRequestMode get_request_mode_vfunc() const override;
+            void get_preferred_width_vfunc(int& minimum_width,
+                                           int& natural_width) const override;
+            void get_preferred_height_vfunc(int& minimum_height,
+                                            int& natural_height) const override;
+            void get_preferred_height_for_width_vfunc(
+                    int width,
+                    int& minimum_height,
+                    int& natural_height) const override;
+    };
+}
+#endif
