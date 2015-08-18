de0829d448ad08ca27f45d0e6a468fd001bdf7eb
Luca Béla Palkovics <luca.bela.palkovics@gmail.com>
2015-08-18 22:51:06 +0200

    Add file resume after client restart test

--- src/tox/test/file.t.h
+++ src/tox/test/file.t.h
@@ -16,6 +16,9 @@ class TestFile : public CxxTest::TestSuite
 {
     public:
         void test_file() {
+            gfix.wait_for_online();
+            gfix.wait_for_contact();
+
             auto file = Gio::File::create_for_path("/tmp/send_test_filet.txt");
             auto stream = file->append_to();
             stream->truncate(0);
@@ -31,9 +34,11 @@ class TestFile : public CxxTest::TestSuite
             int recv_size;
             TOX_FILE_KIND recv_kind;
             bool recv_complete;
+            std::string recv_path;
             sigc::connection con3, con4;
             sigc::connection con1 = gfix.contact_b->file_manager()->signal_recv_file().connect([&](std::shared_ptr<toxmm::file>& file) {
                 recv_name = file->property_name().get_value();
+                recv_path = file->property_path().get_value();
                 recv_size = file->property_size().get_value();
                 recv_kind = file->property_kind().get_value();
                 ++count;
@@ -72,7 +77,8 @@ class TestFile : public CxxTest::TestSuite
             TS_ASSERT_EQUALS(send_complete, true);
             TS_ASSERT_EQUALS(recv_complete, true);
             //check if same content
-            auto rstream = file->read();
+            auto rfile = Gio::File::create_for_path(recv_path);
+            auto rstream = rfile->read();
             char buffer[1024];
             gsize size;
             rstream->read_all((void*)buffer, sizeof(buffer), size);
@@ -80,6 +86,9 @@ class TestFile : public CxxTest::TestSuite
         }
 
         void test_file_abort() {
+            gfix.wait_for_online();
+            gfix.wait_for_contact();
+
             auto file = Gio::File::create_for_path("/tmp/send_test_filet.txt");
             int count = 0;
             bool recv_complete;
@@ -116,5 +125,90 @@ class TestFile : public CxxTest::TestSuite
             TS_ASSERT_EQUALS(recv_complete, true);
             TS_ASSERT_EQUALS(file->query_exists(), true);
             TS_ASSERT_EQUALS(Gio::File::create_for_path(recv_path)->query_exists(), true);
+        }
+
+        void test_file_continue_after_restart() {
+            gfix.wait_for_online();
+            gfix.wait_for_contact();
+
+            TS_TRACE("CAR: SEND FILE");
+            auto file = Gio::File::create_for_path("/tmp/send_test_filet.txt");
+            std::shared_ptr<toxmm::file> original_send_file;
+            std::shared_ptr<toxmm::file> original_recv_file;
+            sigc::connection con1 = gfix.contact_b->file_manager()->signal_recv_file().connect([&](std::shared_ptr<toxmm::file>& file) {
+                original_recv_file = file;
+            });
+            //send file
+            original_send_file = gfix.contact_a->file_manager()->send_file(file->get_path());
+            gfix.wait_while([&]() {
+                return !original_recv_file;
+            });
+            TS_ASSERT(original_recv_file != nullptr);
+            con1.disconnect();
+            if (original_recv_file == nullptr) {
+                return;
+            }
+
+            auto o_send_state = original_send_file->property_state().get_value();
+            auto o_send_remote_state = original_send_file->property_state_remote().get_value();
+            auto o_recv_state = original_recv_file->property_state().get_value();
+            auto o_recv_remote_state = original_recv_file->property_state_remote().get_value();
+
+            //restart
+            TS_TRACE("CAR: RESTART CLIENTS");
+            gfix.reset();
+
+            //get old files
+            TS_TRACE("CAR: GET OLD FILES");
+            std::shared_ptr<toxmm::file> after_send_file = gfix.contact_a->file_manager()->find(original_send_file->property_uuid().get_value());
+            std::shared_ptr<toxmm::file> after_recv_file = gfix.contact_b->file_manager()->find(original_recv_file->property_uuid().get_value());
+            TS_ASSERT(after_send_file != nullptr);
+            TS_ASSERT(after_recv_file != nullptr);
+            if (after_send_file == nullptr || after_recv_file == nullptr) {
+                return;
+            }
+
+            TS_ASSERT_EQUALS(after_send_file->property_active().get_value(), false);
+            TS_ASSERT_EQUALS(after_recv_file->property_active().get_value(), false);
+
+            TS_TRACE("CAR: WAIT FOR CONTACTS");
+            gfix.wait_for_online();
+            gfix.wait_for_contact();
+
+            //wait for file
+            TS_TRACE("CAR: WAIT FOR FILES ACTIVE");
+            gfix.wait_while([&]() {
+                return !after_send_file->property_active() ||
+                       !after_recv_file->property_active();
+            });
+            TS_ASSERT_EQUALS(after_send_file->property_active().get_value(), true);
+            TS_ASSERT_EQUALS(after_recv_file->property_active().get_value(), true);
+
+            //resume file
+            TS_TRACE("CAR: RESUME FILE");
+            auto s_send_state = after_send_file->property_state().get_value();
+            auto s_send_remote_state = after_send_file->property_state_remote().get_value();
+            auto s_recv_state = after_recv_file->property_state().get_value();
+            auto s_recv_remote_state = after_recv_file->property_state_remote().get_value();
+            after_recv_file->property_state() = TOX_FILE_CONTROL_RESUME;
+
+            gfix.wait_while([&]() {
+                return !after_send_file->property_complete() ||
+                       !after_recv_file->property_complete();
+            });
+            auto a_send_state = after_send_file->property_state().get_value();
+            auto a_send_remote_state = after_send_file->property_state_remote().get_value();
+            auto a_recv_state = after_recv_file->property_state().get_value();
+            auto a_recv_remote_state = after_recv_file->property_state_remote().get_value();
+            TS_ASSERT_EQUALS(after_send_file->property_complete().get_value(), true);
+            TS_ASSERT_EQUALS(after_recv_file->property_complete().get_value(), true);
+
+            //check content
+            auto rfile = Gio::File::create_for_path(after_recv_file->property_path().get_value());
+            auto rstream = rfile->read();
+            char buffer[1024];
+            gsize size;
+            rstream->read_all((void*)buffer, sizeof(buffer), size);
+            TS_ASSERT_EQUALS("Dummy File", std::string(buffer, size));
         }
 };
