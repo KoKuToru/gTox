0d309b3e52c2f052dd584ecd3d20f7739919e0e3
Luca Béla Palkovics <luca.bela.palkovics@gmail.com>
2015-08-30 12:25:53 +0200

    travis update for publish

---
+++ .travis_publish_win.sh
@@ -0,0 +1,53 @@
+cp -r ./share ./publish/
+cp gtox.exe ./publish/bin/gtox.exe
+
+cd publish
+
+#Fix libharfbuzz-1.dll bug
+cp bin/libharfbuzz-0.dll ./publish/bin/libharfbuzz-1.dll
+cp /bin/libharfbuzz-gobject-0.dll ./publish/bin/libharfbuzz-gobject-1.dll
+cp /bin/libharfbuzz-icu-0.dll ./publish/bin/libharfbuzz-icu-1.dll
+
+#Generate loaders.cache for SVG
+FILE=lib/gdk-pixbuf-2.0/2.10.0/loaders.cache
+echo "\"../lib/gdk-pixbuf-2.0/2.10.0/loaders/libpixbufloader-svg.dll\"" > $FILE
+echo "\"svg\" 2 \"gdk-pixbuf\" \"Scalable Vector Graphics\" \"LGPL\"" >> $FILE
+echo "\"image/svg+xml\" \"image/svg\" \"image/svg-xml\" \"image/vnd.adobe.svg+xml\" \"text/xml-svg\" \"image/svg+xml-compressed\" \"\"" >> $FILE
+echo "\"svg\" \"svgz\" \"svg.gz\" \"\"" >> $FILE
+echo "\" <svg\" \"*    \" 100" >> $FILE
+echo "\" <\!DOCTYPE svg\" \"*             \" 100" >> $FILE
+echo "" >> $FILE
+echo "" >> $FILE
+
+#Remove unused files
+find . -name "*.a" -type f -delete
+find . -name "*.h" -type f -delete
+find . -name "*.c" -type f -delete
+find . -name "*.hpp" -type f -delete
+find . -name "*.cpp" -type f -delete
+find . -name "*.o" -type f -delete
+find . -name "*.pc" -type f -delete
+find . -name "*.m4" -type f -delete
+find . -name "*.sh" -type f -delete
+find . -name "*.spec" -type f -delete
+find . -name "*.cmake" -type f -delete
+find . -name include -exec rm -rf {} \;
+find . -name proc -exec rm -rf {} \;
+rm -rf share/doc
+find . -type d -empty -exec rm -rf {} \;
+
+PKG_VERSION=( set -o pipefail
+       git describe --long --tags 2>/dev/null | sed 's/\([^-]*-g\)/r\1/;s/-/./g' ||
+       printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"
+     )
+
+sudo pacman -S zip --noconfirm
+zip -r9 ../$PUB_FILE.$PKG_VERSION.zip *
+cd ..
+
+#upload to sftp
+if [ ! -z $PUBLISH_KEY ]; then
+    sudo pacman -S curl --noconfirm
+    curl -v -u $PUBLISH_KEY -T $PUB_FILE.$PKG_VERSION.zip $PUBLISH_HOST
+fi
+
