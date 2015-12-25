0da974e707062e7937bda5345a587cec28a06582
Luca Béla Palkovics <luca.bela.palkovics@gmail.com>
2015-12-25 13:16:54 +0100

    use right path

--- .travis_build_xdg_app.sh
+++ .travis_build_xdg_app.sh
@@ -8,4 +8,4 @@ xdg-app install-runtime --user gnome-sdk org.gnome.Platform 3.18 || exit $?
 xdg-app install-runtime --user gnome-sdk org.freedesktop.Platform 1.0 || exit $?
 xdg-app install-runtime --user gnome-sdk org.gnome.Sdk 3.18 || exit $?
 
-xdg-app-builder . ../xdg-app/org.kokutoru.gtox.json || exit $?
+xdg-app-builder . ../../xdg-app/org.kokutoru.gtox.json || exit $?
